5429a765054f9ef8fbd35be7b0855b17cc16cb53
Scott Gigante <scottgigante@gmail.com>
2018-11-30 23:02:29 -0500

    update gtts submodules

---
+++ korean/lib/README.md
@@ -0,0 +1,6 @@
+Sources
+-------
+
+kengdic : https://github.com/scottgigante/kengdic
+gTTS : https://github.com/pndurette/gTTS
+gtts_token : https://github.com/boudewijn26/gTTS-token
--- korean/lib/gtts/__init__.py
+++ korean/lib/gtts/__init__.py
@@ -1,2 +1,5 @@
-from .version import __version__
-from .tts import gTTS
+# -*- coding: utf-8 -*-
+from .version import __version__  # noqa: F401
+from .tts import gTTS, gTTSError
+
+__all__ = ['gTTS', 'gTTSError']
