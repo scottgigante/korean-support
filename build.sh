11099741bb3b446c225e4019fcd4e6a9cb22f832
Scott Gigante <scottgigante@gmail.com>
2021-01-21 14:00:11 -0500

    remove gtts_token

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ cd lib/gTTS
 rm -rf ../../korean/lib/gtts
 cp -r gtts ../../korean/lib/
 cp LICENSE ../../korean/lib/gtts
-cd ../gTTS-token
-cp -r gtts_token ../../korean/lib/gtts
-cp LICENSE ../../korean/lib/gtts/gtts_token
 cd ../kengdic
 rsync -ah python/kengdic/ ../../korean/lib/kengdic/ --delete
 cp LICENSE ../../korean/lib/kengdic
